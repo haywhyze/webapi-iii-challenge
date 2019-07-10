11cf181e03956b8acea10c61a31ef28e20cf9332
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-10 15:06:00 +0100

    chore: implement logger middleware

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-const express = require('express');
+const app = require('./server');
 const postsRouter = require('./posts/postRouter');
 const usersRouter = require('./users/userRouter');
 
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.status(200).send({
-    message: 'Welcome to Lambda blogs',
-  });
-});
-
-app.use('/api/users', usersRouter);
-app.use('/api/posts', postsRouter);
+// app.use('/api/users', usersRouter);
+// app.use('/api/posts', postsRouter);
 
 app.all('*', (req, res) => {
   res.status(404).send({
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +14,10 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log('\nRequest Method ===> ', req.method);
+  console.log('Request URL ===> ', req.url);
+  console.log('Time Request was made ===> ', (new Date()).toUTCString(), '\n');
+  return next()
 };
 
 module.exports = server;
